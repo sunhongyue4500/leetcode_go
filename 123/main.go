8d40afcc8d1b4e65e9394769e4ea1f3042bce11a
Hongyue Sun <sunhongyue4500@gmail.com>
2019-12-08 10:50:22 +0800

    U update 123

--- 123/main.go
+++ 123/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 )
 
@@ -10,6 +11,8 @@ func main() {
 	}
 
 	log.Println(maxProfit(input))
+	fmt.Println(maxProfit2(input))
+	fmt.Println(maxProfit3(input))
 }
 
 func maxProfit(prices []int) int {
@@ -47,4 +50,45 @@ func temp(cur int, max int, secmax int) (int, int) {
 		return max, cur
 	}
 	return max, secmax
+}
+
+// DP
+func maxProfit2(prices []int) int {
+	// boundary conditions
+	if len(prices) < 2 {
+		return 0
+	}
+	k := 2
+	buy := make([]int, k+1)  // 买入k次的最大利润
+	sell := make([]int, k+1) // 卖出k次的最大利润
+	// dp初始化
+	for i := 0; i <= k; i++ {
+		buy[i] = -(1 << 63)
+	}
+	for i := 0; i < len(prices); i++ {
+		for j := 1; j <= k; j++ {
+			buy[j] = max(buy[j], sell[j-1]-prices[i])
+			sell[j] = max(sell[j], buy[j]+prices[i])
+		}
+	}
+	return sell[k]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// 优化的解法
+func maxProfit3(prices []int) int {
+	oneBuy, oneBuyOneSell, twoBuy, twoBuyTwoSell := -(1 << 63), 0, -(1 << 63), 0
+	for i := 0; i < len(prices); i++ {
+		oneBuy = max(oneBuy, -prices[i])
+		oneBuyOneSell = max(oneBuyOneSell, oneBuy+prices[i])
+		twoBuy = max(twoBuy, oneBuyOneSell-prices[i])
+		twoBuyTwoSell = max(twoBuyTwoSell, twoBuy+prices[i])
+	}
+	return twoBuyTwoSell
 }
