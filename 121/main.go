84902cdb265baa45e3b5da3df3b6ca6f131bdcda
Hongyue Sun <sunhongyue4500@gmail.com>
2019-12-09 10:58:45 +0800

    U update 121

--- 121/main.go
+++ 121/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+	test := []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(test))
+	fmt.Println(maxProfit2(test))
+	fmt.Println(maxProfit3(test))
 }
 
 // 1. 暴力，N^2
@@ -24,4 +27,44 @@ func maxProfit(prices []int) int {
 		}
 	}
 	return maxProfit
+}
+
+// 国际站的一种做法
+func maxProfit3(prices []int) int {
+	maxCur, maxSofar := 0, 0
+	for i := 1; i < len(prices); i++ {
+		maxCur = max(0, maxCur+prices[i]-prices[i-1])
+		if maxCur > maxSofar {
+			maxSofar = maxCur
+		}
+	}
+	return maxSofar
+}
+
+// 用DP来做
+func maxProfit2(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+	k := 1
+	buy := make([]int, k+1)
+	sell := make([]int, k+1)
+	// 初始化
+	for i := 0; i <= k; i++ {
+		buy[i] = -(1 << 63)
+	}
+	for i := 0; i < len(prices); i++ {
+		for j := 1; j <= k; j++ {
+			buy[j] = max(buy[j], sell[j-1]-prices[i])
+			sell[j] = max(sell[j], buy[j]+prices[i])
+		}
+	}
+	return sell[k]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
