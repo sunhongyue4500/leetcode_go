61e0be91b8cc2ad53f475d7886b28bfa4fd7ad3d
Hongyue Sun <sunhongyue4500@gmail.com>
2019-12-16 13:48:38 +0800

    U update 1

--- 1/main.go
+++ 1/main.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 }
 
-
+// 1. 暴力
+// 2. 使用hash表
 func twoSum(nums []int, target int) []int {
 	res := []int{}
 	if len(nums) < 1 {
