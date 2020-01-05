7821758f446c6809aa65c7fb735a6a950734f503
Hongyue Sun <sunhongyue4500@gmail.com>
2020-01-05 15:29:10 +0800

    U update 22

--- 22/main.go
+++ 22/main.go
@@ -8,10 +8,13 @@ func main() {
 	fmt.Println(generateParenthesis(0))
 	fmt.Println(generateParenthesis(1))
 	fmt.Println(generateParenthesis(2))
+
+	fmt.Println(generateParenthesis_3(3))
 }
 
 // 1. 使用暴力，对于n对，可以生成2n个位置，对于每一个位置，可以是(或者)
-// 2. 递归
+// 2. DFS
+// 3. BFS
 func generateParenthesis(n int) []string {
 	res := []string{}
 	helper(n, 0, 0, "", &res)
@@ -59,4 +62,46 @@ func helper2(n int, left int, right int, curStr string, res *[]string) {
 		helper(n, left, right-1, curStr+")", res)
 	}
 	// clear status
+}
+
+type Node struct {
+	str         string
+	leftUnUsed  int
+	rightUnUsed int
+}
+
+// 3. BFS
+func generateParenthesis_3(n int) []string {
+	queue := []*Node{&Node{
+		str:         "", // 当前组装的字符串
+		leftUnUsed:  n,  //左括号剩余
+		rightUnUsed: n,  //右括号剩余
+	}}
+	res := []string{}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		// pop
+		queue = queue[1:]
+		if node.leftUnUsed == 0 && node.rightUnUsed == 0 {
+			res = append(res, node.str)
+		}
+
+		if node.leftUnUsed > 0 {
+			queue = append(queue, &Node{
+				str:         node.str + "(",
+				leftUnUsed:  node.leftUnUsed - 1,
+				rightUnUsed: node.rightUnUsed,
+			})
+		}
+		// 右括号还有没用的并且左括号用的必须比右多
+		if node.rightUnUsed > 0 && node.leftUnUsed < node.rightUnUsed {
+			queue = append(queue, &Node{
+				str:         node.str + ")",
+				leftUnUsed:  node.leftUnUsed,
+				rightUnUsed: node.rightUnUsed - 1,
+			})
+		}
+	}
+	return res
 }
